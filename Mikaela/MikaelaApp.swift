da0568b49912b12a99f8d29fe4fb5ff524125fb0
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-04-16 13:09:25 +0200

    Initial Commit

---
+++ Mikaela/MikaelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MikaelaApp.swift
+//  Mikaela
+//
+//  Created by Valtteri Juvonen on 16.4.2024.
+//
+
+import SwiftUI
+
+@main
+struct MikaelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
